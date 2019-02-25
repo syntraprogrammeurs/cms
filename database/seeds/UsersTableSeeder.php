0370b45368883a5ab88e8cc1391aa75ab618530b
Tom <syntraprogrammeurs@gmail.com>
2019-02-25 10:27:46 +0100

    last commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(App\User::class, 50)->create();
+        DB::insert('insert into users(name, email,password) 
+values (?,?,?)', ['Tom', 'vanhoutte.tom@gmail.com', bcrypt(123456)]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,37 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/**invoegen data in database via DB::insert**/
 Route::get('/insert',function(){
    DB::insert('insert into posts(title, content) values 
 (?,?)', ['PHP LARAVEL', 'LARAVEL BEST PHP FRAMEWORK']);
+});
+/**lezen van data in database via DB::select**/
+Route::get('/users', function(){
+   //$users = DB::select('SELECT * FROM USERS WHERE id = ?',
+     //  [1]);
+    $users = DB::select('SELECT * FROM USERS');
+    //dd($users);
+    echo count($users);
+   foreach($users as $user){
+       //hier kan je de velden vanuit de class/tabel aanspreken/
+
+       echo $user->name . ' ' . $user->email . '<br>';
+   }
+});
+
+/**data wijzigen**/
+Route::get('/users/update', function(){
+   $users = DB::update('update users set email = "testerabdcddd@gmail.com"
+WHERE id= ?',[1]);
+
+   //return $users;
+   return redirect('/users');
+
+});
+
+/**data delete**/
+Route::get('/delete', function(){
+   $deletedUser = DB::delete('delete from users where id = ?', [1]);
+   return $deletedUser;
 });
